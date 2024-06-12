a0d940ce1f8f1d8eb2d2ff586547d31ed665a01e
Krishnan Mani <km@foobar.com>
2024-06-12 18:21:01 +0100

    added lambda function example for SQS invocation

---
+++ lambda-functions/on-message/lambda_function.rb
@@ -0,0 +1,19 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+def lambda_handler(event:, context:)
+    event['Records'].each do |message|
+      process_message(message)
+    end
+    puts "done"
+end
+  
+def process_message(message)
+    begin
+      puts "Processed message #{message['body']}"
+      # TODO: Do interesting work based on the new message
+    rescue StandardError => err
+      puts "An error occurred"
+      raise err
+    end
+end
+  
